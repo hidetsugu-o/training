e463d3367817ae2517b5008287e88bb73a00812d
hidetsugu-o <74892038+hidetsugu-o@users.noreply.github.com>
2021-12-23 07:53:33 +0900

    タイムゾーンの設定と、予想最高気温の追加出力

--- today-weather.php
+++ today-weather.php
@@ -1,7 +1,10 @@
 <?php
+date_default_timezone_set('Asia/Tokyo');
+
 $response = file_get_contents('https://weather.tsukumijima.net/api/forecast/city/140010');
 $weather = json_decode($response, true);
 
 echo date("Y/m/d"), "\n";
 echo $weather['title'], "\n";
 echo $weather['forecasts'][0]['telop'], "\n";
+echo "予想最高気温：", $weather['forecasts'][0]['temperature']['max']['celsius'], "度";
