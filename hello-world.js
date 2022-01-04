bb722488ef5390ce4bb80a6dbfa01e11ffa462f3
hidetsugu-o <74892038+hidetsugu-o@users.noreply.github.com>
2022-01-04 10:08:13 +0900

    JSとHTMLでHello World

--- hello-world.js
+++ hello-world.js
@@ -1,5 +1,13 @@
-function greeter(person) {
-    return "Hello " + person;
+function greeter() {
+    // テキストボックスの入力値を取得
+    var text_box = document.getElementById("name");
+    var name = text_box.value;
+
+    // 新たな要素を作成
+    var new_element = document.createElement("h3");
+    new_element.textContent = "Hello " + name;
+
+    // 親要素の子要素として挿入
+    var parent = document.getElementById("body");
+    parent.appendChild(new_element);
 }
-var user = "Hidetsugu";
-console.log(greeter(user));
