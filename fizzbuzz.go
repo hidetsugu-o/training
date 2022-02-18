4abc020fc6262180bcaebd1a252417373f0bc3eb
hidetsugu-o <74892038+hidetsugu-o@users.noreply.github.com>
2022-02-18 09:37:58 +0900

    再起的にfizzbuzz

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -4,20 +4,25 @@ import (
 	"fmt"
 )
 
-func main() {
-	for i := 1; i <= 100; i++ {
+func fizzbuzz(n int, max int) {
+	multiOf3 := (n%3 == 0)
+	multiOf5 := (n%5 == 0)
 
-		multiOf3 := (i%3 == 0)
-		multiOf5 := (i%5 == 0)
-
-		if multiOf3 && multiOf5 {
-			fmt.Println("fizzbuzz")
-		} else if multiOf3 {
-			fmt.Println("fizz")
-		} else if multiOf5 {
-			fmt.Println("buzz")
-		} else {
-			fmt.Println(i)
-		}
+	if multiOf3 && multiOf5 {
+		fmt.Println("fizzbuzz")
+	} else if multiOf3 {
+		fmt.Println("fizz")
+	} else if multiOf5 {
+		fmt.Println("buzz")
+	} else {
+		fmt.Println(n)
 	}
+
+	if n < max {
+		fizzbuzz(n+1, max)
+	}
+}
+
+func main() {
+	fizzbuzz(1, 100)
 }
