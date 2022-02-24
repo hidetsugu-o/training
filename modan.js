bbeacfbe79cfcc3edeecfa4adbbe1e32ecbaa000
hidetsugu-o <74892038+hidetsugu-o@users.noreply.github.com>
2022-02-25 07:53:29 +0900

    モダンJS

---
+++ modan.js
@@ -0,0 +1,10 @@
+const men = ['bob', 'john', 'dex'];
+
+for (let i = 0; i < men.length; i++) {
+    console.log(men[i]);
+}
+
+men.map((name) => console.log(name));
+
+const two = men.filter((name) => name != 'john');
+console.log(two)
