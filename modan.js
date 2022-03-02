a823380c92ae2c27f281c64368457212a8348401
hidetsugu-o <74892038+hidetsugu-o@users.noreply.github.com>
2022-03-02 23:57:05 +0900

    modanjs練習

--- modan.js
+++ modan.js
@@ -7,4 +7,7 @@ for (let i = 0; i < men.length; i++) {
 men.map((name) => console.log(name));
 
 const two = men.filter((name) => name != 'john');
-console.log(two)
+console.log(two);
+
+const demo = (two) => console.log("demoだよ", two);
+demo(two);
